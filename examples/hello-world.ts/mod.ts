bd322ef2c9a74fdd0d31c2551b997522478dbc31
akumarujon <isakulovdev@gmail.com>
2023-10-09 18:14:15 +0500

    get example

--- examples/hello-world.ts/mod.ts
+++ examples/hello-world.ts/mod.ts
@@ -1,5 +1,13 @@
-import { Micro } from "../../src/mod.ts";
+import { Micro } from "../../src/mod.ts"; // Replace with the correct import path
 
 const app = new Micro();
 
-app.route("/hello/", () => "Hello World")
+app.get("/", (_req: Request) => {
+    return new Response("Hello")
+})
+
+app.get("/hello", (_req: Request) => {
+    return new Response("No Hello.")
+})
+
+app.run(3000)
