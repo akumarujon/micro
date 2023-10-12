ecf3365d27da75e9f37fb055806e8a66151d1841
akumaru <isakulovdev@gmail.com>
2023-10-12 20:12:46 +0500

    add

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,5 @@
 // deno-lint-ignore-file no-explicit-any
-import { Routes } from "./types/route.ts";
-import { MicroResponse } from "./types/response.ts";
-import { MicroRequest } from "./types/request.ts";
+import { MicroRequest, MicroResponse, Params, Routes } from "./types.ts";
 
 // deno-lint-ignore prefer-const
 let routes: Routes = { "post": [], "get": [] };
@@ -15,12 +13,31 @@ class Micro {
     routes.post.push({ path, event });
   }
 
-  handle(req: Request) {
-    const path = new URL(req.url).pathname;
+  handle(request: Request) {
+    const path = new URL(request.url).pathname;
     const res = new MicroResponse();
 
+    let req = new MicroRequest(request.url)
     if (req.method == "GET") {
       for (const route of routes.get) {
+        if (route.path.includes(":")) {
+          const parts = route.path.split("/");
+          const coming_path = path.split("/");
+
+          const part = parts.indexOf(
+            parts.filter((part) => part.includes(":"))[0],
+          );
+
+          const key = parts[part].split(":")[1]
+          const value = coming_path[part];
+          
+          route.path = route.path.replace(`:${key}`, value);
+
+          req.params.push(
+            [key, value],
+          );
+        }
+
         if (path == route.path) {
           return route.event(req as MicroRequest, res);
         }
