63833d3525866881d7cc44b3f5f2e5a32b126afa
akumaru <isakulovdev@gmail.com>
2023-10-12 18:21:22 +0500

    added example for `jsonify()`

--- examples/hello-world.ts/mod.ts
+++ examples/hello-world.ts/mod.ts
@@ -1,23 +1,21 @@
-import { Micro } from "../../src/mod.ts"; // Replace with the correct import path
-import { readerFromStreamReader } from "https://deno.land/std/io/mod.ts";
+import { Micro } from "../../src/mod.ts";
+import { MicroResponse, MicroRequest } from "../../src/types/mod.ts";
 
 const app = new Micro();
 
-app.get("/", (_req: Request) => {
-    return new Response("Hello")
+app.get("/", (_req: MicroRequest, res: MicroResponse) => {
+    const response = {"message":"hello"}
+    
+    return res.jsonify(response);
 })
 
-app.get("/hello", (_req: Request) => {
-    return new Response("No Hello.")
+app.post('/', (_req: MicroRequest ,res: MicroResponse) => {
+    return res.send("POST.")
 })
 
-app.post('/', (_req: Request) => {
-    return new Response("POST.")
-})
-
-app.post('/post',(_req: Request) => {
-    console.log(_req.jsonBody)
-    return new Response("POST ROUTE")
+app.post('/post',async(req: MicroRequest, res: MicroResponse) => {
+    const body = await req.json()
+    return res.jsonify(body)
 })
 
 app.run(3000)
