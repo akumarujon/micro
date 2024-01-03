739b43b21624790bc013a0cb20e020f41176a9bd
akumaru <isakulovdev@gmail.com>
2024-01-04 01:05:04 +0500

    example with req params

--- examples/hello-world.ts/mod.ts
+++ examples/hello-world.ts/mod.ts
@@ -1,21 +1,23 @@
 import { Micro } from "../../src/mod.ts";
-import { MicroResponse, MicroRequest } from "../../src/types.ts";
+import { MicroRequest, MicroResponse } from "../../src/types.ts";
 
 const app = new Micro();
 
 app.get("/", (_req: MicroRequest, _res: MicroResponse) => {
-    const response = {"message":"hello"}
-    
-    return _res.jsonify(response);
-})
+  const response = { "message": "hello" };
 
-app.post('/', (_req: MicroRequest ,_res: MicroResponse) => {
-    return _res.send("POST.")
-})
+  return _res.jsonify(response);
+});
 
-app.post('/post',async(_req: MicroRequest, _res: MicroResponse) => {
-    const body = await _req.json()
-    return _res.jsonify(body)
-})
+app.get("/:user", (_req: MicroRequest, _res: MicroResponse) => {
+  console.log(_req.params);
 
-app.run(3000)
+  return _res.send("OK");
+});
+
+app.post("/post", async (_req: MicroRequest, _res: MicroResponse) => {
+  const body = await _req.json();
+  return _res.jsonify(body);
+});
+
+app.run(3000);
