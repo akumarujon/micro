d52ffe9e78fcd7ea6091c65d2ce5036a2fdb32fd
akumaru <isakulovdev@gmail.com>
2024-01-04 01:06:24 +0500

    unfinished todo app

--- examples/todo/mod.ts
+++ examples/todo/mod.ts
@@ -3,12 +3,12 @@ import { MicroRequest, MicroResponse } from "../../src/types.ts";
 
 const app = new Micro();
 
-app.get('/:id/', (req: MicroRequest, res: MicroResponse) => {
-    return res.jsonify(req.params)
-})
+app.get("/:id/", (req: MicroRequest, res: MicroResponse) => {
+  return res.jsonify(req.params);
+});
 
-app.get("/name/:name/", (req:MicroRequest, res:MicroResponse) => {
-    return res.jsonify(req.params)
-})
+app.get("/name/:name/", (req: MicroRequest, res: MicroResponse) => {
+  return res.jsonify(req.params);
+});
 
 app.run(3000);
